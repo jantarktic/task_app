6adb602062a97125a30253764db01d046ccfb2f0
jantarktic <jaydenclark@outlook.com.au>
2022-10-09 21:56:40 -0700

    added viewset and configured URLs

--- checkbox/urls.py
+++ checkbox/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from task_app import views
+
+router = routers.DefaultRouter()
+router.register(r'tasks', views.TaskView, 'task')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
