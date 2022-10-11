936381a961608cc914a7a9e4e330512ac9bc3066
jantarktic <jaydenclark@outlook.com.au>
2022-10-10 22:33:43 -0700

    configured app and modal component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,173 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Modal from "./components/Modal";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewCompleted: false,
+      taskList: [],
+      modal: false,
+      activeItem: {
+        title: "",
+        description: "",
+        created_date: Date(""),
+        due_date: Date(""),
+        completed: false,
+      },
+    };
+  }
+
+  componentDidMount() {
+    this.refreshList();
+  }
+
+  refreshList = () => {
+    axios
+      .get("/api/tasks/")
+      .then((res) => this.setState({ taskList: res.data }))
+      .catch((err) => console.log(err));
+  };
+
+  toggle = () => {
+    this.setState({ modal: !this.state.modal });
+  };
+
+  handleSubmit = (item) => {
+    this.toggle();
+
+    if (item.id) {
+      axios
+        .put(`/api/tasks/${item.id}/`, item)
+        .then((res) => this.refreshList());
+      return;
+    }
+    axios.post("/api/tasks/", item).then((res) => this.refreshList());
+  };
+
+  handleDelete = (item) => {
+    axios.delete(`/api/tasks/${item.id}/`).then((res) => this.refreshList());
+  };
+
+  createItem = () => {
+    const item = {
+      title: "",
+      description: "",
+      due_date: Date(""),
+      completed: false,
+    };
+
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  };
+
+  editItem = (item) => {
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  };
+
+  displayCompleted = (status) => {
+    if (status) {
+      return this.setState({ viewCompleted: true });
+    }
+
+    return this.setState({ viewCompleted: false });
+  };
+
+  renderTabList = () => {
+    return (
+      <div className="nav nav-tabs">
+        <span
+          className={this.state.viewCompleted ? "nav-link active" : "nav-link"}
+          onClick={() => this.displayCompleted(true)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Complete
+        </span>
+        <span
+          className={this.state.viewCompleted ? "nav-link" : "nav-link active"}
+          onClick={() => this.displayCompleted(false)}
+        >
+          Incomplete
+        </span>
+      </div>
+    );
+  };
+
+  renderItems = () => {
+    const { viewCompleted } = this.state;
+    const newItems = this.state.taskList.filter(
+      (item) => item.completed === viewCompleted
+    );
+
+    return newItems.map((item) => (
+      <li
+        key={item.id}
+        className="list-group-item d-flex justify-content-between align-items-center"
+      >
+        <span
+          className={`task-title mr-2 ${
+            this.state.viewCompleted ? "completed-task" : ""
+          }`}
+          title={item.description}
+        >
+          {item.title}
+        </span>
+        <span>{item.description}</span>
+        <span>
+          <b>Date Created: </b>
+          {item.created.substr(0, 10)}
+        </span>
+        <span>
+          <b>Due Date: </b>
+          {item.due_date.substr(0, 10)}
+        </span>
+        <span className="p-3">{item.status}</span>
+        <span>
+          <button
+            className="btn btn-secondary mr-2"
+            onClick={() => this.editItem(item)}
+          >
+            Edit
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={() => this.handleDelete(item)}
+          >
+            Delete
+          </button>
+        </span>
+      </li>
+    ));
+  };
+
+  render() {
+    return (
+      <main className="container">
+        <h1 className="text-white text-uppercase text-center my-4">Task app</h1>
+        <div className="row">
+          <div className="col-md-17 col-sm-10 mx-auto p-0">
+            <div className="card p-3">
+              <div className="mb-4">
+                <button className="btn btn-primary" onClick={this.createItem}>
+                  Add task
+                </button>
+              </div>
+              {this.renderTabList()}
+              <ul className="list-group list-group-flush border-top-0">
+                {this.renderItems()}
+              </ul>
+            </div>
+          </div>
+        </div>
+        {this.state.modal ? (
+          <Modal
+            activeItem={this.state.activeItem}
+            toggle={this.toggle}
+            onSave={this.handleSubmit}
+          />
+        ) : null}
+      </main>
+    );
+  }
 }
 
 export default App;
